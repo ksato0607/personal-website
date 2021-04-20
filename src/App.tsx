4c23e47af157afb2f18155858ea653afd6c5e1ce
Keisuke Sato <caseksato@gmail.com>
2021-04-20 09:15:24 +0900

    add my introduction

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Profile webite - coming soon</h2>
         <img id="profile-image" src={profileImage} alt="profile" />
-        <span>Profile webite - coming soon</span>
+        <span style={{ fontSize: '36px' }}>Keisuke Sato</span>
+        <span style={{ fontSize: '24px', width: '40%' }}>
+          I'm a software engineer who has been interested in interactive projects which bring people, design and
+          technology together. I worked on an interactive projection mapping for Zushi Art Festival in 2020 and I have
+          been building a platform that connects creators together and takes their creativity into the next level.
+        </span>
         <h1>Highlight</h1>
         <ReactPlayer url="https://www.youtube.com/watch?v=CFBeNRFlvdM" />
+        <br />
       </header>
     </div>
   );
