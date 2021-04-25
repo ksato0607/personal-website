9fd285cebb8f5dcae64aca92d91e17e7e03367f5
Keisuke Sato <caseksato@gmail.com>
2021-04-25 22:32:26 +0900

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import './App.css';
 import profileImage from '../src/assets/my-profile-image.jpg';
+import { FaGithub, FaYoutube, FaLinkedin } from 'react-icons/fa';
 
 function App() {
   return (
@@ -15,6 +16,34 @@ function App() {
           technology together. I worked on an interactive projection mapping for Zushi Art Festival in 2020 and I have
           been building a platform that connects creators together and takes their creativity into the next level.
         </span>
+        <div id="icons" style={{ fontSize: '52px', paddingTop: '10px' }}>
+          {/* <Icon url="https://github.com/ksato0607" icon={<FaGithub />} /> */}
+          <a
+            href="https://github.com/ksato0607"
+            target="_blank"
+            rel="noreferrer"
+            style={{ paddingRight: '20px', color: '#ec7309' }}
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCzosx8yNdW6qPyUoKp-VtAg/videos"
+            target="_blank"
+            rel="noreferrer"
+            style={{ padding: '20px', color: '#ec7309' }}
+          >
+            <FaYoutube />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/keisuke-sato/"
+            target="_blank"
+            rel="noreferrer"
+            style={{ padding: '20px', color: '#ec7309' }}
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+
         <h1>Highlight</h1>
         <ReactPlayer url="https://www.youtube.com/watch?v=CFBeNRFlvdM" />
         <br />
@@ -22,5 +51,13 @@ function App() {
     </div>
   );
 }
+
+// function Icon({ url }: any) {
+//   return (
+//     <a href={url} target="_blank" rel="noreferrer" style={{ paddingRight: '20px', color: '#ec7309' }}>
+//       <FaGithub />
+//     </a>
+//   );
+// }
 
 export default App;
