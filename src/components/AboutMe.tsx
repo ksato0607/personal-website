8c41ad79f54c65baa79c009983f6d3ae7b63bab4
Keisuke Sato <caseksato@gmail.com>
2021-09-23 14:12:50 +0900

    add matchhat in project highlight

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -11,8 +11,8 @@ function AboutMe() {
       <span className="is-size-2">Keisuke Sato</span>
       <span className="is-size-5 container is-size-6-mobile is-max-desktop mx-4">
         Software engineer specializes on data aggregation and also interested in interactive projects which bring
-        people, design and technology together. Projects include interactive projection mapping for 2020 Zushi Art
-        Festival and web application development to connect creators together to take their creativity into next level.
+        people, design and technology together. Projects include interactive projection mapping for Zushi Art Festival
+        and web application development to help creators find collaborators easily to take their creativity to next level.
       </span>
       <div id="icons" className="is-size-1">
         <Icon url="https://github.com/ksato0607" reactIcon={<FaGithub />} />
--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
+import matchhatImage from '../assets/matchhat-top-page.png';
+import './Highlight.css';
 
 function Highlight() {
   return (
     <React.Fragment>
-      <h1 className="is-size-2">Highlight</h1>
+      <h1 className="is-size-2">Projects Highlight</h1>
       <ReactPlayer
         url="https://www.youtube.com/watch?v=CFBeNRFlvdM"
         width="800px"
@@ -12,6 +14,15 @@ function Highlight() {
         style={{ height: 'auto', maxWidth: '90%' }}
       />
       <span className="px-2 is-size-6">Interactive video meditation presented in Zushi Art Festival 2020</span>
+
+      <br />
+      <a className='matchhat'href="https://matchhat.com" target="_blank" rel="noreferrer">
+        <img src={matchhatImage} alt="matchhat" />
+      </a>
+      <span className="px-2 is-size-6 matchhat">
+        Web application where users can find collaborators from different backgrounds and fields to realize their
+        passion projects, and take their creativity to the next level.
+      </span>
     </React.Fragment>
   );
 }
