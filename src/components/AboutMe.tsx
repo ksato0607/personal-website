2e193e90974aeda7c7f36542ac1f3c0f87edb9a6
Keisuke Sato <caseksato@gmail.com>
2021-09-23 11:29:04 +0900

    remove unnecessary style

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import Icon from './Icon';
 import { FaGithub, FaYoutube, FaLinkedin } from 'react-icons/fa';
 import profileImage from '../../src/assets/my-profile-image.jpg';
+import './AboutMe.css';
 
 function AboutMe() {
   return (
     <React.Fragment>
       <img id="profile-image" src={profileImage} alt="profile" />
       <span className="is-size-2">Keisuke Sato</span>
-      <span className="is-size-3 container is-size-6-mobile is-max-desktop" style={{ margin: '10px' }}>
+      <span className="is-size-3 container is-size-6-mobile is-max-desktop">
         I'm a software engineer who has been interested in interactive projects which bring people, design and
         technology together. I worked on an interactive projection mapping for Zushi Art Festival in 2020 and I have
         been building a platform that connects creators together and takes their creativity into the next level.
       </span>
-      <div id="icons" style={{ fontSize: '52px', paddingTop: '10px' }}>
+      <div id="icons" className="is-size-1">
         <Icon url="https://github.com/ksato0607" reactIcon={<FaGithub />} />
         <Icon url="https://www.youtube.com/channel/UCzosx8yNdW6qPyUoKp-VtAg/videos" reactIcon={<FaYoutube />} />
         <Icon url="https://www.linkedin.com/in/keisuke-sato/" reactIcon={<FaLinkedin />} />
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,14 +10,9 @@ const wordCloudFontSize: MinMaxPair = isMobile ? [15, 40] : [20, 60];
 function Skills() {
   return (
     <React.Fragment>
-      <h1 className="is-size-2" style={{ marginTop: '10px' }}>
-        Technical Skills
-      </h1>
-      <div className="columns is-mobile is-centered" style={{ margin: '10px' }}>
-        <div
-          className="has-background-white-bis"
-          style={{ background: 'has-background-white-bis', borderRadius: '5%' }}
-        >
+      <h1 className="is-size-2 m-4">Technical Skills</h1>
+      <div className="columns is-mobile is-centered">
+        <div className="has-background-white-bis" style={{ borderRadius: '5%' }}>
           <ReactWordcloud
             options={{
               fontFamily: 'courier new',
