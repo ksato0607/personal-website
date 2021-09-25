0502a96bbaf1823597541dfd79415dbe673f847d
Keisuke Sato <caseksato@gmail.com>
2021-09-25 22:15:17 +0900

    update highlighted project descriptions

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -13,15 +13,19 @@ function Highlight() {
         height="450px"
         style={{ height: 'auto', maxWidth: '90%' }}
       />
-      <span className="px-2 is-size-6">Interactive video meditation presented in Zushi Art Festival 2020</span>
+      <span className="px-2 is-size-6 highlight-project">
+        Interactive projection mapping presented in Zushi Art Festival 2020. Involved as a co-director to generate the
+        concept of the installation, and as an software engineer to develop different effect patterns using Touch Designer & Kinect.
+      </span>
 
       <br />
-      <a className="matchhat" href="https://matchhat.com" target="_blank" rel="noreferrer">
+      <a className="highlight-project" href="https://matchhat.com" target="_blank" rel="noreferrer">
         <img src={matchhatImage} alt="matchhat" />
       </a>
-      <span className="px-2 is-size-6 matchhat">
-        Web application where users can find collaborators from different backgrounds and fields to realize their
-        passion projects, and take their creativity to the next level.
+      <span className="px-2 is-size-6 highlight-project">
+        Global platform where users can find collaborators from different backgrounds and fields to realize their
+        passion projects. Involved as a full-stack engineer to add new features on frontend & backend, and also
+        introduced end-to-end testing framework and monitoring system to the platform.
       </span>
     </React.Fragment>
   );
