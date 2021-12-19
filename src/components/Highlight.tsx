265779d7179691967881ead89030d2654e744711
Keisuke Sato <caseksato@gmail.com>
2021-12-19 10:25:18 +0900

    add the latest work, spiral dance, to highlight

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -8,7 +8,21 @@ function Highlight() {
     <React.Fragment>
       <h1 className="is-size-2">Projects Highlight</h1>
       <ReactPlayer
-        url="https://www.youtube.com/watch?v=CFBeNRFlvdM"
+        url="https://youtu.be/ZSBIcO_cLRM"
+        controls={true}
+        width="800px"
+        height="450px"
+        style={{ height: 'auto', maxWidth: '90%' }}
+      />
+      <span className="px-2 is-size-6 highlight-project">
+        Interactive projection mapping presented in Zushi Art Festival 2021. Involved as a co-director to brainstorm the
+        concept, coordinate a collaboration with a Unity engineer who had built a boids algorithm, and as an software
+        engineer to integrate the algorithm with Kinect on Unity.
+      </span>
+      <br />
+
+      <ReactPlayer
+        url="https://youtu.be/CFBeNRFlvdM"
         controls={true}
         width="800px"
         height="450px"
@@ -19,8 +33,8 @@ function Highlight() {
         concept of the installation, and as an software engineer to develop different effect patterns using Touch
         Designer & Kinect.
       </span>
-
       <br />
+
       <a className="highlight-project" href="https://matchhat.com" target="_blank" rel="noreferrer">
         <img className="shadow" src={matchhatImage} alt="matchhat" />
       </a>
