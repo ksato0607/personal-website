d4901b7d9cf3217da91c181c6dfac9be18bf462e
Keisuke Sato <caseksato@gmail.com>
2022-01-23 17:58:20 +0900

    update the description of personal projects

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -7,7 +7,7 @@ import './Highlight.css';
 function Highlight() {
   return (
     <React.Fragment>
-      <span className="is-size-2">Projects Highlight</span>
+      <span className="is-size-2">Personal Projects Highlight</span>
       <ReactPlayer
         url="https://youtu.be/ZSBIcO_cLRM"
         controls={true}
@@ -17,9 +17,9 @@ function Highlight() {
         style={{ height: 'auto', maxWidth: '90%' }}
       />
       <span className="px-2 is-size-6 highlight-project">
-        Interactive projection mapping presented in Zushi Art Festival 2021. Involved as a co-director to brainstorm the
-        concept, coordinate a collaboration with a Unity engineer who had built a boids algorithm, and as an software
-        engineer to integrate the algorithm with Kinect on Unity.
+        Interactive media art presented in Zushi Art Festival 2021. Involved as a co-director to brainstorm the concept,
+        coordinate a collaboration with a Unity engineer who had built a boids algorithm, and as an software engineer to
+        integrate the algorithm with Kinect on Unity.
       </span>
       <br />
 
@@ -32,9 +32,9 @@ function Highlight() {
         style={{ height: 'auto', maxWidth: '90%' }}
       />
       <span className="px-2 is-size-6 highlight-project">
-        Interactive projection mapping presented in Zushi Art Festival 2020. Involved as a co-director to generate the
-        concept of the installation, and as an software engineer to develop different effect patterns using Touch
-        Designer & Kinect.
+        Interactive media art presented in Zushi Art Festival 2020. Involved as a co-director to brainstorm the concept
+        of the installation, and as a software engineer to develop different effect patterns using TouchDesigner &
+        Kinect.
       </span>
       <br />
 
@@ -43,8 +43,9 @@ function Highlight() {
       </a>
       <span className="px-2 is-size-6 highlight-project">
         Global platform where users can find collaborators from different backgrounds and fields to realize their
-        passion projects. Involved as a full-stack engineer to add new features on frontend & backend, and also
-        introduced end-to-end testing framework and monitoring system to the platform.
+        passion projects. As a founding member & full-stack engineer, improved the web application from frontend,
+        backend, database and deployment. Also introduced end-to-end testing framework and monitoring system to the
+        platform to enhance the reliability and the observability of the application.
       </span>
     </React.Fragment>
   );
